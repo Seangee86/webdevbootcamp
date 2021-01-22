876b31ec6e7c30c04498c3e6ecafa550bd2217b8
Sean Granier <seanlg624@gmail.com>
2021-01-22 11:02:16 -0600

    practice

---
+++ Documents/Webdevbootcamp/app.js
@@ -0,0 +1,44 @@
+// let msg = "haha that is so funny!"
+
+// console.log(msg.slice(10, 12))
+
+
+// let annoyingLaugh = "teehee so funny! teehee!";
+// console.log(annoyingLaugh.replace('teehee', 'poop'));
+
+// let funny = "lol";
+// console.log(funny.repeat(19));
+
+// DON'T TOUCH THIS LINE! (please)
+// const word = "skateboard"; //Don't change this line!
+
+// // YOUR CODE BELOW THIS LINE:
+// let facialHair = word.slice(5, 10).replace("o", "e");
+
+// console.log(facialHair);
+
+// NO TOUCHING! (please)
+// const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+// const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// // YOUR CODE BELOW THIS LINE:
+// let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
+
+// console.log(roll);
+
+// if (1 + 1 === 2) {
+//     console.log("math is smart");
+// }
+
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+  if (num % 2 === 0) {
+      console.log("Even");
+  }
+    //AND THIS LINE ↑↑↑↑↑
+}
+console.log(isEven(24));
+
+
+
+
